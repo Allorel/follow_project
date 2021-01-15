eaf0201089dacaef3a61c7eb5ec092dc97c475fa
allorel <morelbodjrenou@gmail.com>
2021-01-15 19:51:36 +0100

    Utilisation  bumdle vich uploader symfony pour ajouter les fichiers

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -6,11 +6,14 @@ use App\Entity\Traits\Timestampable;
 use App\Repository\PinRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 
 /**
  * @ORM\Entity(repositoryClass=PinRepository::class)
  * @ORM\Table(name="pins")
+ * @Vich\Uploadable
  * @ORM\HasLifecycleCallbacks
  */
 class Pin
@@ -81,6 +84,28 @@ class Pin
 
         return $this;
     }
+
+
+    /**
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->setUpdateAt(new \DateTimeImmutable) ;
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+
 
     public function getCreateAt(): ?\DateTimeInterface
     {
